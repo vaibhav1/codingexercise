0ce5f6b82fb4c8eadce2a3a9c9615cf90f89e739
Vaibhav Shukla <vaibhav2008in@gmail.com>
2016-07-27 16:08:58 +0530

    updated problem statement in comments

--- LookAndSay.java
+++ LookAndSay.java
@@ -1,7 +1,21 @@
 /**
+ * PROBLEM:
+ * 
+ * LookAndSay : Define a function as follows: Read of the digits of input counting the number of digits in groups of same digit.
+ * 
+ * Example:
+ *    	lookAndSay(1) 	- 	"11"  	Because it reads "one 1" or 11
+ * 	lookAndSay(11) 	-	"21"  	Because it reads "two 1s" or 21
+ * 	lookAndSay(21)	-	"1211"	Because it reads off as "one 2 one 1" or 1211
+ * 
+ * In similar way we generate the LookAndSay String for any given number. 
+ * Now, given a number 'start' and iteration 'n' , calculate the nth number in LookAndSay sequence starting with 'start'.
+ * 
+ * Using the above example : if start = 11 and n=2 then lookAndSay(11,2) = lookAndSay(lookAndSay(11)) = "1211"
+ * 
  * 
  */
-package com.expedia.code;
+package com.demo.code;
 
 /**
  * @author vshukla
@@ -10,6 +24,9 @@ package com.expedia.code;
 public class LookAndSay {
 	
 	
+	/**
+	 * Methods takes in a start and iteration n and returns the nth string in LookandSay sequence starting with start.
+	 */ 
 	public static String lookAndSay(int start, int n){
 		
 		String num = String.valueOf(start);
@@ -21,6 +38,9 @@ public class LookAndSay {
 	}
 	
 
+	/**
+	 * Method converts a given string into its lookandsay string.
+	 */ 
 	private static String lookAndSayString(String number) {
 		StringBuilder result= new StringBuilder();
 		 
